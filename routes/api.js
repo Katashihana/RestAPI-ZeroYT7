ba7eabdc07b97b75dde1005daf5bbd4ae3235c95
Katashihana <89012832+Katashihana@users.noreply.github.com>
2021-12-06 01:16:33 +0700

    Add files via upload

--- routes/api.js
+++ routes/api.js
@@ -47,12 +47,6 @@ var { recognize } = require(__path + '/lib/ocr.js')
 var options = require(__path + '/lib/options.js');
 var { Vokal, Base, Searchnabi, Gempa } = require('./../lib');
 var {
-  palingmurah_,
-  layarkaca_,
-  Amino_,
-} = require("./../lib/search");
-
-var {
   jalantikus,
   turnbackhoax,
   muihalal,
@@ -63,10 +57,6 @@ var {
   mcpedl,
   pstore,
 } = require("./../lib/tod");
-var {
-  exports.pornvid,
-  exports.hentaivid,
-} = require("./../lib/scrapper");
 
 var cookie = "HSID=A7EDzLn3kae2B1Njb;SSID=AheuwUjMojTWvA5GN;APISID=cgfXh13rQbb4zbLP/AlvlPJ2xBJBsykmS_;SAPISID=m82rJG4AC9nxQ5uG/A1FotfA_gi9pvo91C;__Secure-3PAPISID=m82rJG4AC9nxQ5uG/A1FotfA_gi9pvo91C;VISITOR_INFO1_LIVE=RgZLnZtCoPU;LOGIN_INFO=AFmmF2swRQIhAOXIXsKVou2azuz-kTsCKpbM9szRExAMUD-OwHYiuB6eAiAyPm4Ag3O9rbma7umBK-AG1zoGqyJinh4ia03csp5Nkw:QUQ3MjNmeXJ0UHFRS3dzaTNGRmlWR2FfMDRxa2NRYTFiN3lfTEdOVTc4QUlwbUI4S2dlVngxSG10N3ZqcHZwTHBKano5SkN2dDlPSkhRMUtReE42TkhYeUVWS3kyUE1jY2I1QzA1MDZBaktwd1llWU9lOWE4NWhoZV92aDkxeE9vMTNlcG1uMU9rYjhOaDZWdno2ZzN3TXl5TVNhSjNBRnJaMExrQXpoa2xzRVUteFNWZDI5S0Fn;PREF=app=desktop&f4=4000000&al=id;SID=2wezCMTUkWN3YS1VmS_DXaEU84J0pZIQdemM8Zry-uzWm8y1njBpLTOpxSfN-EaYCRSiDg.;YSC=HCowA1fmvzo;__Secure-3PSID=2wezCMTUkWN3YS1VmS_DXaEU84J0pZIQdemM8Zry-uzWm8y1dajgWzlBh9TgKapGOwuXfA.;SIDCC=AJi4QfFK0ri9fSfMjMQ4tOJNp6vOb9emETXB_nf2S05mvr2jBlmeEvlSsQSzPMuJl_V0wcbL1r8;__Secure-3PSIDCC=AJi4QfGeWHx-c4uTpU1rXCciO1p0s2fJWU07KrkZhWyD1Tqi8LyR-kHuBwHY9mViVYu1fRh2PA";
 
@@ -979,35 +969,6 @@ router.get("/search/servermc", async(req, res, next) => {
 			res.sendFile(error)
 		})
 })
-
-router.get("/search/hentaivid", async(req, res, next) => {
-  apikeyInput = req.query.apikey;
-  if (!apikeyInput) return res.json(loghandler.notparam)
-		if (apikeyInput !== `${key}`) return res.sendFile(invalidKey)
-  exports.hentaivid
-    .then((resolve) => {
-      res.json
-    })
-  .catch(e => {
-			console.log('Error :', color(e, 'red'))
-			res.sendFile(error)
-		})
-})
-
-router.get("/search/ttporn", async(req, res, next) => {
-  apikeyInput = req.query.apikey;
-  if (!apikeyInput) return res.json(loghandler.notparam)
-		if (apikeyInput !== `${key}`) return res.sendFile(invalidKey)
-  exports.pornvid
-    .then((resolve) => {
-      res.json
-    })
-  .catch(e => {
-			console.log('Error :', color(e, 'red'))
-			res.sendFile(error)
-		})
-})
-
 router.get('/infogempa', async (req, res, next) => {
 	        var apikeyInput = req.query.apikey
 

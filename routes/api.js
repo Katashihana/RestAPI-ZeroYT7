377494c024c9a500e2e96ee3aa193aa313981834
Katashihana <89012832+Katashihana@users.noreply.github.com>
2021-12-06 14:57:19 +0700

    Add files via upload

--- routes/api.js
+++ routes/api.js
@@ -57,6 +57,17 @@ var {
   mcpedl,
   pstore,
 } = require("./../lib/tod");
+var {
+  otakudesu,
+  covid,
+  sholat,
+  playstore,
+  igdl,
+  igstory,
+  igstalk,
+  fbdown,
+  twitter,
+} = require("./../lib/downloadig2");
 
 var cookie = "HSID=A7EDzLn3kae2B1Njb;SSID=AheuwUjMojTWvA5GN;APISID=cgfXh13rQbb4zbLP/AlvlPJ2xBJBsykmS_;SAPISID=m82rJG4AC9nxQ5uG/A1FotfA_gi9pvo91C;__Secure-3PAPISID=m82rJG4AC9nxQ5uG/A1FotfA_gi9pvo91C;VISITOR_INFO1_LIVE=RgZLnZtCoPU;LOGIN_INFO=AFmmF2swRQIhAOXIXsKVou2azuz-kTsCKpbM9szRExAMUD-OwHYiuB6eAiAyPm4Ag3O9rbma7umBK-AG1zoGqyJinh4ia03csp5Nkw:QUQ3MjNmeXJ0UHFRS3dzaTNGRmlWR2FfMDRxa2NRYTFiN3lfTEdOVTc4QUlwbUI4S2dlVngxSG10N3ZqcHZwTHBKano5SkN2dDlPSkhRMUtReE42TkhYeUVWS3kyUE1jY2I1QzA1MDZBaktwd1llWU9lOWE4NWhoZV92aDkxeE9vMTNlcG1uMU9rYjhOaDZWdno2ZzN3TXl5TVNhSjNBRnJaMExrQXpoa2xzRVUteFNWZDI5S0Fn;PREF=app=desktop&f4=4000000&al=id;SID=2wezCMTUkWN3YS1VmS_DXaEU84J0pZIQdemM8Zry-uzWm8y1njBpLTOpxSfN-EaYCRSiDg.;YSC=HCowA1fmvzo;__Secure-3PSID=2wezCMTUkWN3YS1VmS_DXaEU84J0pZIQdemM8Zry-uzWm8y1dajgWzlBh9TgKapGOwuXfA.;SIDCC=AJi4QfFK0ri9fSfMjMQ4tOJNp6vOb9emETXB_nf2S05mvr2jBlmeEvlSsQSzPMuJl_V0wcbL1r8;__Secure-3PSIDCC=AJi4QfGeWHx-c4uTpU1rXCciO1p0s2fJWU07KrkZhWyD1Tqi8LyR-kHuBwHY9mViVYu1fRh2PA";
 
@@ -368,7 +379,6 @@ router.get('/tiktod/stalk', async (req, res, next) => {
              })
          })
 })
-
 
 router.get('/infonpm', async (req, res, next) => {
         var apikeyInput = req.query.apikey,
@@ -923,6 +933,70 @@ router.get("/search/jalantikus", async(req, res, next) => {
   .catch(e => {
 			console.log('Error :', color(e, 'red'))
 			res.sendFile(error)
+		})
+})
+
+router.get("/media/fbdown2", async(req, res, next) => {
+  apikeyInput = req.query.apikey;
+   url = req.query.url
+  if (!url) return res.json(loghandler.noturl)
+  if (!apikeyInput) return res.json(loghandler.notparam)
+		if (apikeyInput !== `${key}`) return res.sendFile(invalidKey)
+  fbdown(url)
+    .then((data) => {
+      res.json(data)
+    })
+  .catch(e => {
+			console.log('Error :', color(e, 'red'))
+			res.json(loghandler.invalidLink)
+		})
+})
+
+router.get("/media/igstalk", async(req, res, next) => {
+  apikeyInput = req.query.apikey;
+   username = req.query.username
+  if (!username) return res.json(loghandler.notusername)
+  if (!apikeyInput) return res.json(loghandler.notparam)
+		if (apikeyInput !== `${key}`) return res.sendFile(invalidKey)
+  igstalk(username)
+    .then((data) => {
+      res.json(data)
+    })
+  .catch(e => {
+			console.log('Error :', color(e, 'red'))
+			res.sendFile(error)
+		})
+})
+
+router.get("/media/igstory", async(req, res, next) => {
+  apikeyInput = req.query.apikey;
+   username = req.query.username
+  if (!username) return res.json(loghandler.notusername)
+  if (!apikeyInput) return res.json(loghandler.notparam)
+		if (apikeyInput !== `${key}`) return res.sendFile(invalidKey)
+  igstory(username)
+    .then((data) => {
+      res.json(data)
+    })
+  .catch(e => {
+			console.log('Error :', color(e, 'red'))
+			res.sendFile(error)
+		})
+})
+
+router.get("/media/igdl", async(req, res, next) => {
+  apikeyInput = req.query.apikey;
+   url = req.query.url
+  if (!url) return res.json(loghandler.noturl)
+  if (!apikeyInput) return res.json(loghandler.notparam)
+		if (apikeyInput !== `${key}`) return res.sendFile(invalidKey)
+  igdl(url)
+    .then((data) => {
+      res.json(data)
+    })
+  .catch(e => {
+			console.log('Error :', color(e, 'red'))
+			res.json(loghandler.invalidLink)
 		})
 })
 

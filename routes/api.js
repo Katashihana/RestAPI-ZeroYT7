2f50bf5e3fc5e6f2a29161d23e9ce3d32bbb2242
Katashihana <89012832+Katashihana@users.noreply.github.com>
2021-12-10 17:12:46 +0700

    Add files via upload

--- routes/api.js
+++ routes/api.js
@@ -31,7 +31,6 @@ var fetch = require('node-fetch');
 var cheerio = require('cheerio');
 var request = require('request');
 var TikTokScraper = require('tiktok-scraper');
-var nekopoi = require('nekobocc');
 var yts = require('yt-search');
 var fs = require('fs');
 var util = require('util');
@@ -70,6 +69,12 @@ var {
   fbdown,
   twitter,
 } = require("./../lib/downloadig2");
+var {
+  latest,
+  getHentaiEpisode,
+  search,
+  getHentai,
+} = require("./../lib/NekoBocc");
 var {
   asahotakasahotak, family100,  siapakah, siapakah2, susunkata, tekateki } = require("./../lib/scrapegame");
 var cookie = "HSID=A7EDzLn3kae2B1Njb;SSID=AheuwUjMojTWvA5GN;APISID=cgfXh13rQbb4zbLP/AlvlPJ2xBJBsykmS_;SAPISID=m82rJG4AC9nxQ5uG/A1FotfA_gi9pvo91C;__Secure-3PAPISID=m82rJG4AC9nxQ5uG/A1FotfA_gi9pvo91C;VISITOR_INFO1_LIVE=RgZLnZtCoPU;LOGIN_INFO=AFmmF2swRQIhAOXIXsKVou2azuz-kTsCKpbM9szRExAMUD-OwHYiuB6eAiAyPm4Ag3O9rbma7umBK-AG1zoGqyJinh4ia03csp5Nkw:QUQ3MjNmeXJ0UHFRS3dzaTNGRmlWR2FfMDRxa2NRYTFiN3lfTEdOVTc4QUlwbUI4S2dlVngxSG10N3ZqcHZwTHBKano5SkN2dDlPSkhRMUtReE42TkhYeUVWS3kyUE1jY2I1QzA1MDZBaktwd1llWU9lOWE4NWhoZV92aDkxeE9vMTNlcG1uMU9rYjhOaDZWdno2ZzN3TXl5TVNhSjNBRnJaMExrQXpoa2xzRVUteFNWZDI5S0Fn;PREF=app=desktop&f4=4000000&al=id;SID=2wezCMTUkWN3YS1VmS_DXaEU84J0pZIQdemM8Zry-uzWm8y1njBpLTOpxSfN-EaYCRSiDg.;YSC=HCowA1fmvzo;__Secure-3PSID=2wezCMTUkWN3YS1VmS_DXaEU84J0pZIQdemM8Zry-uzWm8y1dajgWzlBh9TgKapGOwuXfA.;SIDCC=AJi4QfFK0ri9fSfMjMQ4tOJNp6vOb9emETXB_nf2S05mvr2jBlmeEvlSsQSzPMuJl_V0wcbL1r8;__Secure-3PSIDCC=AJi4QfGeWHx-c4uTpU1rXCciO1p0s2fJWU07KrkZhWyD1Tqi8LyR-kHuBwHY9mViVYu1fRh2PA";
@@ -1157,7 +1162,7 @@ router.get('/nekopoii/latest', async (req, res, next) => {
 	if(apikeyInput !== `${key}`) return res.sendFile(invalidKey)
      
 
-     nekopoi.latest()
+     latest()
          .then(format => {
              console.log(format)
              res.json({
@@ -1181,7 +1186,7 @@ router.get('/nekopoii/search', async (req, res, next) => {
 	if(apikeyInput !== `${key}`) return res.sendFile(invalidKey)
      if(!query) return res.json(loghandler.notquery)
 
-     nekopoi.search(query)
+     search(query)
          .then(format => {
              console.log(format)
              res.json({
@@ -1205,7 +1210,7 @@ router.get('/nekopoii/getHentaiEpisode', async (req, res, next) => {
 	if(apikeyInput !== `${key}`) return res.sendFile(invalidKey)
      if (!url) return res.json(loghandler.noturl)
 
-     nekopoi.getHentaiEpisode(url)
+     getHentaiEpisode(url)
          .then(format => {
              console.log(format)
              res.json({
@@ -1230,7 +1235,7 @@ router.get('/nekopoii/getHentai', async (req, res, next) => {
 	if(apikeyInput !== `${key}`) return res.sendFile(invalidKey)
      if (!url) return res.json(loghandler.noturl)
 
-     nekopoi.getHentai(url)
+     getHentai(url)
          .then(format => {
              console.log(format)
              res.json({
@@ -1242,30 +1247,6 @@ router.get('/nekopoii/getHentai', async (req, res, next) => {
          .catch(e => {
 			console.log('Error :', color(e, 'red'))
 			res.json(loghandler.invalidLink)
-		})
-})
-
-
-router.get('/nekopoii/random', async (req, res, next) => {
-    var apikeyInput = req.query.apikey,
-
-
-	if(!apikeyInput) return res.json(loghandler.notparam)
-	if(apikeyInput !== `${key}`) return res.sendFile(invalidKey)
-     
-
-     nekopoi.random()
-         .then(format => {
-             console.log(format)
-             res.json({
-                 status: true,
-                 creator: `${creator}`,
-                 videoNoWm: format
-             })
-         })
-         .catch(e => {
-			console.log('Error :', color(e, 'red'))
-			res.sendFile(error)
 		})
 })
 
